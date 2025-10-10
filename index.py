d612fc117f1eaf1f5792aa53483141ec5d985547
Gabriel Lima <49318475+yuusha-sama@users.noreply.github.com>
2025-10-09 21:49:52 -0300

    teste

---
+++ index.py
@@ -0,0 +1,17 @@
+from pylab import *
+from rtlsdr import *
+
+sdr = RtlSdr()
+
+sdr.sample_rate = 2.4e6
+sdr.center_freq = 95e6
+sdr.gain = 4
+
+samples = sdr.read_samples(256*1024)
+sdr.close()
+
+psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
+xlabel('Frequency (MHz)')
+ylabel('Relative power (dB)')
+
+show()
